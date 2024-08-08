fbb6ad67d9f123b9ad40afc12afbab976645b0d7
Vasily Styagov <styagov.dev@gmail.com>
2024-08-08 13:21:49 +0100

    docs: add product description (#3)
    * add docs for api
    
    * add docs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-source dev.env
-
 make build
 
 if [ $? -ne 0 ]; then
@@ -9,6 +7,5 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo ">> Deploying contract"
-
-near dev-deploy --wasmFile "res/sweat_booster.wasm" --initFunction "init" --initArgs "{\"ft_account_id\": \"$TOKEN_ACCOUNT_ID\"}"
+cd contract
+cargo near deploy
